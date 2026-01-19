2732e1752ff2874775420110275d25e05d8d76df
onma6334 <onma6334@gmail.com>
2026-01-19 23:16:20 +0900

    unzip spring initializer

---
+++ src/main/java/com/example/docker_ap_ci/DockerApCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_ap_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerApCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/docker_ap_ci/DockerApCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker_ap_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
